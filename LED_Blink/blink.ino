aad276ea0342e50b7dd61efb89a3bcfa413d9afc
sourabh-robot-code <yadavsourabh546@gmail.com>
2026-03-21 18:04:59 +0530

    Update blink.ino

--- LED_Blink/blink.ino
+++ LED_Blink/blink.ino
@@ -1,19 +1,3 @@
-# Arduino Uno Beginner Projects
-This repository contains my beginner Arduino learning experiments.
-
-## Project 1 – LED Blink
-This project blinks an LED connected to pin 12.
-
-  ### Components
-- Arduino Uno
-- LED
-- 220Ω resistor
-- Breadboard
-- Jumper wires
-
-### Circuit
-Pin 12 → Resistor → LED → GND
-
   ##Code
 void setup()
 {
@@ -27,6 +11,4 @@ void loop()
   digitalWrite(12, LOW);
   delay(200);
 }
-### Code
-The LED turns ON and OFF every 200 milliseconds.
 
